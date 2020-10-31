264c44921e19a5bcd7d7bdf0d9bb772576214d89
Emilio Schepis <emilio.schepis@gmail.com>
2020-10-31 14:17:58 +0100

    feat: revert back to default google fonts links
    Next.js 10 provides automatic optimization of Google Fonts.
    https://github.com/vercel/next.js/pull/14746

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,10 @@ export default class extends Document {
             title="RSS feed for blog posts"
             href="https://emilioschepis.com/rss.xml"
           />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Fugaz+One&family=Montserrat:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
